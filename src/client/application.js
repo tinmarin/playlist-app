9de137c2ef371776692e077c934a1210521a395b
Sergio Marron <sergiolmarron@gmail.com>
2016-07-18 00:54:41 -0400

    First commit

---
+++ src/client/application.js
@@ -0,0 +1,25 @@
+import "./application.scss";
+
+import * as services from "./services";
+
+//----------------
+// Playground
+
+services.server.emitAction$("login", { username: "foo", password : "bar"})
+    .subscribe( user => {
+        console.log("We're logged in: " + user);
+    }, error => {
+        console.error(error);
+    });
+     
+//----------------
+// Auth
+
+//----------------
+// Componnets
+
+
+//----------------
+// Bootstrap
+
+services.socket.connect(); 
